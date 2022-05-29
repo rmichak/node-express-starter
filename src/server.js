ae882927bd7667bbd6c894a6196de8ab83f1f03b
Randy Michak <rmichak@gmail.com>
2022-05-29 16:10:55 +0000

    initial Commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { routes } from './routes';
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// This allows us to access the body of POST/PUT
+// requests in our route handlers (as req.body)
+app.use(express.json());
+
+// Add all the routes to our Express server
+// exported from routes/index.js
+routes.forEach(route => {
+    app[route.method](route.path, route.handler);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
