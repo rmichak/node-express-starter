eb5e515dbad040a3a1b6b47174494690b5b5c407
Randy Michak <rmichak@gmail.com>
2023-02-25 22:08:42 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { routes } from './routes';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(cors());
 // This allows us to access the body of POST/PUT
 // requests in our route handlers (as req.body)
 app.use(express.json());
